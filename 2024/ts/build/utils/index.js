9eaba1800dc59a492fdfca092ef002c82ae86c14
christopher <christopher.harold.butler@gmail.com>
2024-11-29 01:24:35 +0100

    some typescript setup

--- 2024/ts/build/utils/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.dataFileReader = void 0;
-var fileUtils_1 = require("./fileUtils");
-Object.defineProperty(exports, "dataFileReader", { enumerable: true, get: function () { return fileUtils_1.dataFileReader; } });
