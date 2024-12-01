4228bf09d1bff486ae5b3be56b8f537a10616df1
christopher <christopher.harold.butler@gmail.com>
2024-12-01 22:44:41 +0100

    day01 - typescript

--- 2024/ts/src/index.ts
+++ 2024/ts/src/index.ts
@@ -10,11 +10,11 @@ console.log(`AoC: ${currentDate}`);
 console.log('-----------------\n');
 
 // test boilerplate
-const testDate = "01-12-2000";
-boilerplate.solvePart1(testDate, true);
-boilerplate.solvePart2(testDate);
+// const testDate = "01-12-2000";
+// boilerplate.solvePart1(testDate, true);
+// boilerplate.solvePart2(testDate);
 
 // run puzzle solvers
 
-// day01.solvePart1(currentDate, true);
-// day01.solvePart2(currentDate);
+day01.solvePart1(currentDate); // ans: 3508942
+day01.solvePart2(currentDate); // ans: 26593248
