85899a56164fa3dc89696aa4538ac9c309552b33
christopher <christopher.harold.butler@gmail.com>
2025-12-04 21:21:52 +0100

    day 3 & 4 - go

--- 2025/go/utils/file_utils.go
+++ 2025/go/utils/file_utils.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 func DataFileReader(date string, useTestData bool, part string, verbose bool) ([]string, error) {
@@ -55,4 +56,57 @@ func DataFileReader(date string, useTestData bool, part string, verbose bool) ([
 	}
 
 	return lines, nil
+}
+
+func DataFileToMatrix(date string, useTestData bool, part string, verbose bool) ([][]string, error) {
+
+	currentDir, err := os.Getwd()
+
+	if err != nil {
+		fmt.Println("Error getting current directory:", err)
+		return nil, err
+	}
+	if verbose {
+		fmt.Println("Current directory:", currentDir)
+	}
+
+	parentDir := filepath.Dir(currentDir)
+
+	if verbose {
+		fmt.Println("Parent directory:", parentDir)
+	}
+
+	var fullpath string
+	if useTestData {
+		fullpath = parentDir + "/common/data/" + date + "-part" + part + "-test-data.txt"
+	} else {
+		fullpath = parentDir + "/common/data/" + date + "-data.txt"
+	}
+
+	if verbose {
+		fmt.Println("Full path:", fullpath)
+	}
+
+	file, err := os.Open(fullpath)
+
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var matrix [][]string
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Split(line, "")
+		row := append([]string(nil), parts...)
+		matrix = append(matrix, row)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return matrix, nil
 }
